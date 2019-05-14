dee9a5ab275b159ff1a4aea64476895f2b693357
Anton Bracke <anton.bracke.com@gmail.com>
2019-05-14 22:11:27 +0200

    added area controller and view content

--- src/main/java/zeiterfassung/controllers/AreaController.java
+++ src/main/java/zeiterfassung/controllers/AreaController.java
@@ -1,16 +1,35 @@
 package zeiterfassung.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import zeiterfassung.models.Area;
 
 public class AreaController {
 
     private Area area;
 
+    @FXML
+    TextField name;
+
+    @FXML
+    TextArea description;
+
     public void setArea(Area area) {
         this.area = area;
+
+        // update gui elements
+        name.setText(area.getName());
+        description.setText(area.getDescription());
     }
 
-    public Area getArea() {
-        return area;
+    @FXML
+    public void setName() {
+        area.setName(name.getText());
+    }
+
+    @FXML
+    public void setDescription() {
+        area.setDescription(description.getText());
     }
 }
