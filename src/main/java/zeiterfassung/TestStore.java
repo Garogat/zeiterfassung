34bdb392c5d321d568e01b5e3b3675cae4656f35
Anton Bracke <anton.bracke.com@gmail.com>
2019-05-14 22:16:49 +0200

    expanded TestStore

--- src/main/java/zeiterfassung/TestStore.java
+++ src/main/java/zeiterfassung/TestStore.java
@@ -3,9 +3,10 @@ package zeiterfassung;
 import zeiterfassung.models.*;
 
 public class TestStore extends DataStore {
-    @Override
-    public TimeRegistrationRoot getRoot() {
-        TimeRegistrationRoot root = new TimeRegistrationRoot();
+    TimeRegistrationRoot root;
+
+    public TestStore() {
+        root = new TimeRegistrationRoot();
 
         Area area = new Area();
         area.setName("Privat");
@@ -26,6 +27,26 @@ public class TestStore extends DataStore {
         task.setDescription("Nur die schönen dicken Äpfel dürfen gepflückt werden.");
         subProject.addTask(task);
 
+        SubProject subProject1 = new SubProject();
+        subProject1.setName("Hühner");
+        project.addSubProject(subProject1);
+
+        Task task1 = new Task();
+        task1.setName("Stall ausmisten");
+        subProject.addTask(task1);
+
+        Task task2 = new Task();
+        task2.setName("Eier sammeln");
+        subProject.addTask(task2);
+
+        Project project1 = new Project();
+        project1.setName("FH");
+        project1.setDescription("Dieses Projekt ist für meine FH Aufgaben");
+        area.addProject(project1);
+    }
+
+    @Override
+    public TimeRegistrationRoot getRoot() {
         return root;
     }
 }
