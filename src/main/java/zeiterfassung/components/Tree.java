6fd612fb11579dfe33fe65c9fc51ecf546b7918d
Anton Bracke <anton.bracke.com@gmail.com>
2019-05-15 11:17:33 +0200

    fixed Tree to be persistent

--- src/main/java/zeiterfassung/components/Tree.java
+++ src/main/java/zeiterfassung/components/Tree.java
@@ -1,44 +1,37 @@
 package zeiterfassung.components;
 
-import javafx.event.Event;
 import javafx.scene.control.TreeItem;
 import zeiterfassung.models.*;
-
-import java.util.Observable;
-import java.util.Observer;
 
 public class Tree {
 
     private TimeRegistrationRoot root;
+    private TreeItem<TreeContextItem> rootItem;
 
     public Tree(TimeRegistrationRoot root) {
         this.root = root;
+
+        build();
     }
 
     public TreeItem<TreeContextItem> getTree() {
-        TreeItem<TreeContextItem> rootItem = new TreeItem<TreeContextItem>(new TreeContextItem(this.root));
+        return rootItem;
+    }
+
+    public void build() {
+        rootItem = new TreeItem<>(new TreeContextItem(this.root));
         rootItem.setExpanded(true);
 
         this.root.getAreas(list -> {
             for (Area area : list) {
                 TreeItem<TreeContextItem> item = getArea(area);
-                area.addObserver(new Observer() {
-                    @Override
-                    public void update(Observable observable, Object o) {
-                        System.out.println("tt:" + ((Area) observable).getName());
-                        TreeItem.TreeModificationEvent<TreeContextItem> event = new TreeItem.TreeModificationEvent<>(TreeItem.valueChangedEvent(), item);
-                        Event.fireEvent(item, event);
-                    }
-                });
                 rootItem.getChildren().add(item);
             }
         });
-
-        return rootItem;
     }
 
     private TreeItem<TreeContextItem> getArea(Area area) {
-        TreeItem<TreeContextItem> rootItem = new TreeItem<TreeContextItem>(new TreeContextItem(area));
+        TreeItem<TreeContextItem> rootItem = new TreeItem<>(new TreeContextItem(area));
         rootItem.setExpanded(true);
 
         area.getProjectList(projects -> {
@@ -52,19 +45,19 @@ public class Tree {
     }
 
     private TreeItem<TreeContextItem> getProject(Project project) {
-        TreeItem<TreeContextItem> rootItem = new TreeItem<TreeContextItem>(new TreeContextItem(project));
+        TreeItem<TreeContextItem> rootItem = new TreeItem<>(new TreeContextItem(project));
         rootItem.setExpanded(true);
 
         project.getSubProjects(subProjects -> {
             for (SubProject subProject : subProjects) {
-                TreeItem item = getSubProject(subProject);
+                TreeItem<TreeContextItem> item = getSubProject(subProject);
                 rootItem.getChildren().add(item);
             }
         });
 
         project.getTasks(tasks -> {
             for (Task task : tasks) {
-                TreeItem<TreeContextItem> item = new TreeItem<TreeContextItem>(new TreeContextItem(task));
+                TreeItem<TreeContextItem> item = new TreeItem<>(new TreeContextItem(task));
                 rootItem.getChildren().add(item);
             }
         });
@@ -73,12 +66,12 @@ public class Tree {
     }
 
     private TreeItem<TreeContextItem> getSubProject(SubProject subProject) {
-        TreeItem<TreeContextItem> rootItem = new TreeItem<TreeContextItem>(new TreeContextItem(subProject));
+        TreeItem<TreeContextItem> rootItem = new TreeItem<>(new TreeContextItem(subProject));
         rootItem.setExpanded(true);
 
         subProject.getTasks(tasks -> {
             for (Task task : tasks) {
-                TreeItem<TreeContextItem> item = new TreeItem<TreeContextItem>(new TreeContextItem(task));
+                TreeItem<TreeContextItem> item = new TreeItem<>(new TreeContextItem(task));
                 rootItem.getChildren().add(item);
             }
         });
