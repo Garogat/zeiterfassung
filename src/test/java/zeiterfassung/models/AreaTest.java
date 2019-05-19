985f7da41301ae936701ba5c1fe6b02c8161c906
Christina Schramm <christina.schramm@student.fh-kiel.de>
2019-05-19 20:54:45 +0200

    added Area tests

--- src/test/java/zeiterfassung/models/AreaTest.java
+++ src/test/java/zeiterfassung/models/AreaTest.java
@@ -1,36 +1,84 @@
 package zeiterfassung.models;
 
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
+
 public class AreaTest {
 
-    @org.junit.Test
+    private List<String> projects1 = new ArrayList<>();
+    private Area areaTest1 = new Area();
+
+    private Area areaTest2 = new Area();
+
+    private Area areaTest3 = new Area();
+
+
+    /**
+     * Test for getting the Area name
+     */
+    @Test
     public void getName() {
+        areaTest1.setName("Garten");
+        areaTest2.setName("Fachhochschule");
+        areaTest3.setName("Privat");
+
+        assertEquals("Garten", areaTest1.getName());
+        assertEquals("Fachhochschule", areaTest2.getName());
+        assertEquals("Privat", areaTest3.getName());
     }
 
-    @org.junit.Test
+    @Test
     public void setName() {
     }
 
-    @org.junit.Test
+
+    /**
+     * Test for getting the Area description
+     */
+    @Test
     public void getDescription() {
+        areaTest1.setDescription("Alles, was mit dem Garten zu tun hat.");
+        areaTest2.setDescription("Bereich Studium");
+        areaTest3.setDescription("Was zu Hause zu tun ist");
+
+        assertEquals("Alles, was mit dem Garten zu tun hat.", areaTest1.getDescription());
+        assertEquals("Bereich Studium", areaTest2.getDescription());
+        assertEquals("Was zu Hause zu tun ist", areaTest3.getDescription());
     }
 
-    @org.junit.Test
+    @Test
     public void setDescription() {
     }
 
-    @org.junit.Test
+    @Test
     public void addProject() {
     }
 
-    @org.junit.Test
+    /**
+     * Test for getting a list of projects
+     */
+    @Test
     public void getProject() {
+        fillList(projects1, 5, "Hecke schneiden");
+        assertEquals(5, projects1.size());
     }
 
-    @org.junit.Test
+    //filling the projects list for testing
+    private void fillList(List<String> projects1, int howMany, String name) {
+        for (int i = 0; i < howMany; i++){
+            projects1.add(name + i);
+        }
+    }
+
+
+    /**
+     * Test for checking if an area has a project
+     */
+    @Test
     public void hasProject() {
-    }
-
-    @org.junit.Test
-    public void hasProject1() {
     }
 }
