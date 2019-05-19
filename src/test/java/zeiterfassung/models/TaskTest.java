dc0ac96f489446669aca38111c41c2c4ec0d31cc
Christina Schramm <christina.schramm@student.fh-kiel.de>
2019-05-19 21:45:23 +0200

    added tests for tasks

--- src/test/java/zeiterfassung/models/TaskTest.java
+++ src/test/java/zeiterfassung/models/TaskTest.java
@@ -2,16 +2,33 @@ package zeiterfassung.models;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class TaskTest {
 
+    private Task taskTest1 = new Task();
+
+    private Task taskTest2 = new Task();
+
+    /**
+     * Test for checking if the name is returned correctly
+     */
     @Test
     public void getName() {
+        taskTest1.setName("Hausaufgaben");
+        taskTest2.setName("Putzen");
+
+        assertEquals("Hausaufgaben", taskTest1.getName());
+        assertEquals("Putzen", taskTest2.getName());
     }
 
     @Test
     public void setName() {
     }
 
+    /**
+     * Test for checking if the role is returned correctly
+     */
     @Test
     public void getRole() {
     }
@@ -20,8 +37,16 @@ public class TaskTest {
     public void setRole() {
     }
 
+    /**
+     * Test for checking if the description is returned correctly
+     */
     @Test
     public void getDescription() {
+        taskTest1.setDescription("Zu erledigen fuer FH");
+        taskTest2.setDescription("");
+
+        assertEquals("Zu erledigen fuer FH", taskTest1.getDescription());
+        assertEquals("", taskTest2.getDescription());
     }
 
     @Test
