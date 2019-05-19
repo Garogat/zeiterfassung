e4efb27eac9d2aa4d939dc741710524498db2951
Christina Schramm <christina.schramm@student.fh-kiel.de>
2019-05-19 21:26:20 +0200

    added some tests for subproject

--- src/test/java/zeiterfassung/models/SubProjectTest.java
+++ src/test/java/zeiterfassung/models/SubProjectTest.java
@@ -2,18 +2,44 @@ package zeiterfassung.models;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class SubProjectTest {
 
+    private List<String> taskList1 = new ArrayList<>();
+    private SubProject subProjectTest1 = new SubProject();
+
+    private SubProject subProjectTest2 = new SubProject();
+
+    /**
+     * Test for checking if the name is returned correctly
+     */
     @Test
     public void getName() {
+        subProjectTest1.setName("Gruenpflege");
+        subProjectTest2.setName("Aufraeumen");
+
+        assertEquals("Gruenpflege", subProjectTest1.getName());
+        assertEquals("Aufraeumen", subProjectTest2.getName());
     }
 
     @Test
     public void setName() {
     }
 
+    /**
+     * Test for checking if the description is returned correctly
+     */
     @Test
     public void getDescription() {
+        subProjectTest1.setDescription("Um Gruenkram im Garten kuemmern");
+        subProjectTest2.setDescription("nicht toll");
+
+        assertEquals("Um Gruenkram im Garten kuemmern", subProjectTest1.getDescription());
+        assertEquals("nicht toll", subProjectTest2.getDescription());
     }
 
     @Test
