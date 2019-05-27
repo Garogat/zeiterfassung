2e9e321084fe131c356456fb47b021159242fc56
Anton Bracke <anton.bracke.com@gmail.com>
2019-05-27 16:46:56 +0200

    area: added report create button

--- src/main/java/zeiterfassung/controllers/AreaController.java
+++ src/main/java/zeiterfassung/controllers/AreaController.java
@@ -3,7 +3,15 @@ package zeiterfassung.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import zeiterfassung.models.Area;
+import zeiterfassung.reports.AreaContent;
+import zeiterfassung.reports.ReportDocument;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
 
 public class AreaController {
 
@@ -21,5 +29,26 @@ public class AreaController {
         // update gui elements
         name.textProperty().bindBidirectional(area.nameProperty());
         description.textProperty().bindBidirectional(area.descriptionProperty());
+    }
+
+    @FXML
+    private void createReport() {
+        // TODO: get time
+        LocalDateTime date = LocalDateTime.now();
+
+        ReportDocument report = new ReportDocument("Stundenzettel", new AreaContent(this.area, date.minusMonths(1), date));
+        String html = report.getHtmlNode().getHTMLCode();
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Studenzettel speichern");
+        File file = fileChooser.showSaveDialog(name.getScene().getWindow());
+
+        try {
+            PrintWriter writer = new PrintWriter(file);
+            writer.println(html);
+            writer.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/zeiterfassung/models/Area.java
+++ src/main/java/zeiterfassung/models/Area.java
@@ -1,11 +1,12 @@
 package zeiterfassung.models;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement
