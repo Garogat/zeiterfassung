1862da642928a9b39e11d396955293a71bfcff0f
Christina Schramm <christina.schramm@student.fh-kiel.de>
2019-06-09 23:01:29 +0200

    added basic html style for "Rechnung erstellen"

--- src/main/java/zeiterfassung/reports/ProjectInvoice.java
+++ src/main/java/zeiterfassung/reports/ProjectInvoice.java
@@ -2,6 +2,7 @@ package zeiterfassung.reports;
 
 import htmlProducer.HtmlElement;
 import htmlProducer.HtmlTagElement;
+import htmlProducer.HtmlValuePair;
 import zeiterfassung.Utils;
 import zeiterfassung.models.Project;
 
@@ -17,15 +18,22 @@ public class ProjectInvoice implements Reportable {
 
     private Project project;
 
+    HtmlValuePair border = new HtmlValuePair("style", "border: 1px solid black");
+    HtmlValuePair backgroundColor = new HtmlValuePair("style", "color: light-grey");
+    HtmlValuePair fontFamily = new HtmlValuePair("style", "font-family: Arial Black");
+    HtmlValuePair fontColor = new HtmlValuePair("style", "color: red");
+    HtmlValuePair fontColor2 = new HtmlValuePair("style", "color: green");
+
+
     @Override
     public HtmlElement getHtmlNode() {
         HtmlTagElement root = SPAN.build();
 
         // Caption
-        root.addElement(H4.build().addText(project.getName()), BR.build());
+        root.addElement(H4.build().addProperty(fontFamily).addText(project.getName()), BR.build());
 
         // Customer Line by Line
-        root.addElement(H4.build().addText("Auftraggeber")).addElement(BR.build());
+        root.addElement(H4.build().addProperty(fontFamily).addText("Auftraggeber")).addElement(BR.build());
         if (project.getCustomer() != null) {
             Scanner scanner = new Scanner(project.getCustomer());
             while (scanner.hasNextLine()) {
@@ -38,20 +46,20 @@ public class ProjectInvoice implements Reportable {
 
         root.addElement(BR.build(), BR.build(), BR.build(), BR.build());
 
-        root.addElement(TABLE.build().addElement(
+        root.addElement(TABLE.build().addProperty(border).addElement(
 
-                TR.build().addElement(
-                        TD.build().addText("Projekt:"),
-                        TD.build().addText(project.getName())
+                TR.build().addProperty(border).addElement(
+                        TD.build().addProperty(fontFamily).addText("Projekt:"),
+                        TD.build().addProperty(fontColor2).addText(project.getName())
                 ),
 
                 TR.build().addElement(
-                        TD.build().addText("Dauer:"),
-                        TD.build().addText(Utils.formatDuration(project.getDuration(LocalDateTime.MIN, LocalDateTime.MAX)))
+                        TD.build().addProperty(fontFamily).addText("Dauer:"),
+                        TD.build().addProperty(fontColor2).addText(Utils.formatDuration(project.getDuration(LocalDateTime.MIN, LocalDateTime.MAX)))
                 ),
                 TR.build().addElement(
-                        TD.build().addText("Kosten:"),
-                        TD.build().addText(Utils.formatCosts(project.getCosts(LocalDateTime.MIN, LocalDateTime.MAX)))
+                        TD.build().addProperty(fontFamily).addText("Kosten:"),
+                        TD.build().addProperty(fontColor).addText(Utils.formatCosts(project.getCosts(LocalDateTime.MIN, LocalDateTime.MAX)))
                 )
         ));
 
